8a20ca0d3c4e3c188159782552dad7bffd1c3480
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2022-12-01 23:56:43 +0400

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void foo() throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        com.mysql.jdbc.Driver s;
+
+
+    }
+}
